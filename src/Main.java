4b431b881ab1b87f0f6d74fc88d29bc760932b2f
Carlos Mendez Sanchez <mendez.sanchez.carlos@iescamas.es>
2024-03-22 18:46:35 +0100

    Principal2.2

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,17 @@ public class Main {
 		numero2 = sc.nextInt();
 		resultado = producto(numero1, numero2);
 		System.out.println("Producto: " + resultado);
-
+		resultado = dividir(numero1, numero2);
+		System.out.println("Dividir: " + resultado);
 	}
 	public static int producto(int a, int b) {
 		int c;
 		c = a * b;
+		return c;
+		}
+	public static int dividir(int a, int b) {
+		int c;
+		c = a / b;
 		return c;
 		}
 }
